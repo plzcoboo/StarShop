b513054de5448d2ad93cd999d683718b64e496f0
plzcobooo <plzcobooo@gmail.com>
2024-01-20 23:29:29 +0900

    'footer-css'

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,4 +1,10 @@
 import { FooterWrap } from "./FooterStyle";
+import { FaTiktok } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
 
  
 const Footer = () => {
@@ -6,6 +12,33 @@ const Footer = () => {
         <FooterWrap>
             <div className="inner">
                 {/* <h2>하단로고</h2>   */}
+                <div className="menu-social">
+                    <nav className="navi">
+                        <div className="nav-title">More From Star Wars</div>
+                        <ul className="brand-icon">
+                            <li><FaTiktok /></li>
+                            <li><FiInstagram /></li>
+                            <li><RiTwitterXFill /></li>
+                            <li><FaFacebook /></li>
+                            <li><FaYoutube /></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="copy">
+                <strong>HH & © HyunKiyunfilm Ltd. All Rights Reserved</strong>
+                <ul className="Terms">
+                    <li>Terms of Use</li>
+                    <li>Additional Content Information</li>
+                    <li>Privacy Policy</li>
+                    <li>Children's Online Privacy Policy</li>
+                    <li>Your US State Privacy Rights</li>
+                    <li>Star Wars at shopDisney</li>
+                    <li>Star Wars Helpdesk</li>
+                    <li>Interest-Based Ads</li>
+                </ul>
+                <p>Do Not Sell or Share My Personal Information</p>
+                </div>
+             
             </div>
         </FooterWrap>
     );
--- src/footer/FooterStyle.js
+++ src/footer/FooterStyle.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components'
  
 export const FooterWrap = styled.footer`
+color: #E3F2FD;
+    padding-bottom:50px;
     background-color:#213067;
-    .inner { height:250px;}
+    .inner {}
     h2 { padding-top:80px; font-size:20px; color:#fff }
+    .menu-social{width:200px; margin:auto; text-align:center; padding-top:50px;}
+    .brand-icon{display:flex; justify-content:space-between; svg{font-size:25px;}}
+    .nav-title{margin-bottom:20px;}
+    .navi{margin-bottom:20px;}
+    .copy{text-align: center; p{font-size:13px; margin-bottom:20px}}
+    .Terms{display:flex; margin-top:20px; margin-bottom:20px; li{font-size:13px;text-decoration: underline; text-underline-offset:5px;} justify-content:space-between}
 `
