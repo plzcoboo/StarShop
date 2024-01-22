68836b504cf0281f213af5196f55f09447f7b6b7
plzcobooo <plzcobooo@gmail.com>
2024-01-22 23:21:01 +0900

    'dist경로수정'

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,15 +1,14 @@
-
 import {  Link  } from "react-router-dom";
 import NavBar from "./NavBar"; 
 import { HeaderWrap } from "./HeaderStyle";
-import logo from '/images/sw_logo.jpg'
+
 
 const Header = () => {
      
     return (
         <HeaderWrap>
             <div className="inner">
-                <h1><Link to={"/"}>  <img src={logo} alt=""  /> </Link></h1>
+                <h1><Link to={"/"}>  <img src='./images/sw_logo.jpg' alt="logo" /> </Link></h1>
                 <NavBar />
             </div>
         </HeaderWrap>
